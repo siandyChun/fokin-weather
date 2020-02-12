6b961aa1440b268ef37b1c0a957d6df4c414b8ec
전석인 <andy-chun@jeonseog-in-ui-MacBookPro.local>
2020-02-12 16:13:37 +0900

    commi all

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js!</Text>
+      <Text>Hello!</Text>
     </View>
   );
 }
